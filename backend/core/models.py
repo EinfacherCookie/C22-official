1b5ccc9f337a263785c2eb96acf2048dde9e6901
Cookie <cookie.einfacher@gmail.com>
2023-09-13 15:52:06 +0200

    First commit

---
+++ backend/core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
